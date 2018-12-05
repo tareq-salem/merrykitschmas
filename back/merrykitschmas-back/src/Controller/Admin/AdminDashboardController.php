39ebbba4eec2e037c8d66a7b1863b3139dce64d1
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-12-05 16:36:09 +0100

    add WIP on Admin dashboard (#16)

---
+++ back/merrykitschmas-back/src/Controller/Admin/AdminDashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminDashboardController extends AbstractController
+{
+    /**
+     * @Route("/admin/dashboard", name="admin_dashboard")
+     */
+    public function index()
+    {
+        return $this->render('admin_dashboard/index.html.twig', [
+            'controller_name' => 'AdminDashboardController',
+        ]);
+    }
+}
