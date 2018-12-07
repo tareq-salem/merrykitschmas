54f0970ecf07fef6a7d826d909e9e311caf66c3d
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-12-07 15:27:44 +0100

    Dev back (#33)
    * create new project symfony
    
    * entities et repositories (#15)
    
    * add WIP on Admin dashboard (#16)
    
    * Add Fixtures for create Datas with php bin/console doctrine:fixtures:… (#19)
    
    * Add Fixtures for create Datas with php bin/console doctrine:fixtures:load and bugfix relations
    
    * Add composer.lock
    
    * Sq land datas (#20)
    
    * Add Fixtures for create Datas with php bin/console doctrine:fixtures:load and bugfix relations
    
    * Add composer.lock
    
    * remove .idea from project
    
    * update gitignore (#21)
    
    * Tareq/category product (#24)
    
    * entities et repositories
    
    * controllerCategory et ControllerProduct et SubcategoryController
    
    * add gitignore
    
    * update gitignore
    
    * controllerCategory et ControllerProduct et SubcategoryController
    
    * add gitignore
    
    * get all : products, categories, subcategories and themes
    
    * Cors (#28)
    
    * BugFix Cors
    
    * BugFix Cors
    
    * "delete env."
    
    * Fix .env.test
    
    * create new project symfony
    
    * Delete modules.xml
    
    * Delete misc.xml
    
    * Delete merrykitschmas.iml
    
    * Delete workspace.xml
    
    * fuck idea
    
    * add WIP on Admin dashboard (#16)
    
    * Add Fixtures for create Datas with php bin/console doctrine:fixtures:… (#19)
    
    * Add Fixtures for create Datas with php bin/console doctrine:fixtures:load and bugfix relations
    
    * Add composer.lock
    
    * Sq land datas (#20)
    
    * Add Fixtures for create Datas with php bin/console doctrine:fixtures:load and bugfix relations
    
    * Add composer.lock
    
    * remove .idea from project
    
    * update gitignore (#21)
    
    * Tareq/category product (#24)
    
    * entities et repositories
    
    * controllerCategory et ControllerProduct et SubcategoryController
    
    * add gitignore
    
    * update gitignore
    
    * controllerCategory et ControllerProduct et SubcategoryController
    
    * add gitignore
    
    * get all : products, categories, subcategories and themes
    
    * Cors (#28)
    
    * BugFix Cors
    
    * BugFix Cors
    
    * Fix .env.test

--- back/merrykitschmas-back/src/Entity/Category.php
+++ back/merrykitschmas-back/src/Entity/Category.php
@@ -40,6 +40,11 @@ class Category
         return $this->name;
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
